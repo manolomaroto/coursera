9c68221cd5836e0270020d141d62b06c8314c628
manuel gonzalez <manuelgmaroto@gmail.com>
2016-09-20 11:51:33 +0200

    Finalizado primer tema segunda semana

---
+++ week2/FiltersDigestCycleDataBinding/lecture16/app.js
@@ -0,0 +1,27 @@
+(function(){
+    'use strict';
+    angular.module('BindingApp',[])
+        .controller('BindingController',BindingController);
+
+    BindingController.$inject = ['$scope'];
+    function BindingController($scope){
+        $scope.firstName = "Manolo";
+        //$scope.fullName = "";
+
+        $scope.showNumberOfWatchers = function(){
+            console.log("# of watchers: ", $scope.$$watchersCount);
+        }
+
+        $scope.setFullName = function(){
+            $scope.fullName = $scope.firstName + " " + "Maroto";
+        }
+
+        $scope.logFirstName = function(){
+            console.log("First name is: ", $scope.firstName);
+        }
+
+        $scope.logFullName = function(){
+            console.log("Full name is: ", $scope.fullName);
+        }
+    }
+})();
